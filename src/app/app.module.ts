329661843332b70d8553d51d086a2c75f711ac2c
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-06-25 11:46:41 +0530

    1 Layer routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { WatchesComponent } from './watches/watches.component';
+import { AccessoriesComponent } from './accessories/accessories.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'watches', component: WatchesComponent },
+  { path: 'accessories', component: AccessoriesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    WatchesComponent,
+    AccessoriesComponent,
+    NotfoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
